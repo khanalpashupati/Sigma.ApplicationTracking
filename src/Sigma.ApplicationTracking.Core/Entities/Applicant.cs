e592ae16c933a39952a9d7568839c1137472630b
khanalpashupati <khanal.pashupati@gmail.com>
2025-01-16 21:07:31 +0545

    Add project files.

---
+++ src/Sigma.ApplicationTracking.Core/Entities/Applicant.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sigma.ApplicationTracking.Core.Entities
+{
+    public class Applicant
+    {
+        [Key]
+        [StringLength(255)]
+        public string Email { get; set; } = string.Empty;
+
+        [StringLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [StringLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [StringLength(50)]
+        public string? PhoneNumber { get; set; } =string.Empty;
+
+        public DateTime? PreferredTime { get; set; }
+
+        [StringLength(255)]
+        public string? LinkedInProfileUrl { get; set; } = string.Empty;
+
+        public string Comment {  get; set; } = string.Empty;
+
+
+
+//• * First name
+//• * Last name
+//• Phone number
+//• * Email
+//• Time interval when it’s better to call(in case a call is needed)
+//• LinkedIn profile URL
+//• GitHub profile URL
+//• * Free text comment
+    }
+}
